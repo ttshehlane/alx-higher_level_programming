a8621b4b3306c3955d83a2aa573795b874c42458
Thuba Tshehlane <ttshehlane@outlook.com>
2023-11-02 16:18:31 -0700

    fixed duplicated value for zero

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@ from sys import argv
 if __name__ == "__main__":
     sum = 0
     if len(argv) <= 1:
-        print("0")
+        print("{}".format(sum))
     else:
         for c in range(1, len(argv)):
             sum += int(argv[c])
-    print("{}".format(sum))
+        print("{}".format(sum))
