fe2fffc4572267819e752e2e99f7d12a14d106d4
Thuba Tshehlane <ttshehlane@outlook.com>
2023-11-06 04:52:57 -0800

    making a shallow copy and also editting the value at a given element on the copy list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
         return None
     num_el = len(my_list)
     if idx < 0 or idx >= num_el or num_el == 0:
-        return my_list.copy
+        return my_list[:]
     temp_list = my_list[:]
     temp_list[idx] = element
     return temp_list
