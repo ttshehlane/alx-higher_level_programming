a39b1124eb43b9f5eef1a0d11514418d93fa963c
Thuba Tshehlane <ttshehlane@outlook.com>
2023-11-06 15:32:30 -0800

    counting characters and returning the first char of the string

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    str_len = 0
+    if sentence is None:
+        return (str_len, None)
+    str_len = len(sentence)
+    return (str_len, sentence[0])
