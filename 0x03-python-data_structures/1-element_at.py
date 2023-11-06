e43760ace530c08baf5e9f4dfa9a2752afb43589
Thuba Tshehlane <ttshehlane@outlook.com>
2023-11-06 03:51:56 -0800

    fixed the error hadnling when invalid inputs are added to inputs

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    num_el = len(my_list)
-    if idx < 0 or idx >= num_el:
-        return none
+    num_el = 0
+    if my_list is not None:
+        num_el = len(my_list)
+    if idx < 0 or idx >= num_el or num_el == 0:
+        return None
     return my_list[idx]
