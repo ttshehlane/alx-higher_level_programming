a9dd2c14855a2d37f9bdf2ba97aa5c58cdbbc941
Thuba Tshehlane <ttshehlane@outlook.com>
2023-11-07 11:56:46 -0800

    i had mistakenly made my first condition <= 1 and not zero

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) <= 1:
+    if len(my_list) <= 0:
         return None
     max_val = my_list[0]
     for a in range(len(my_list)):
